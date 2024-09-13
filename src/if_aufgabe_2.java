6f8c2593f2c871740d581df852578e1fb62cb75f
moritz12455 <moritz12455>
2024-09-13 13:01:18 +0200

    Added IF Aufgabe 1

---
+++ src/if_aufgabe_2.java
@@ -0,0 +1,20 @@
+import java.util.Random;
+
+public class if_aufgabe_2 {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int randomnumber1 = random.nextInt(100);
+        int randomnumber2 = random.nextInt(100);
+
+        System.out.println(randomnumber1);
+        System.out.println(randomnumber2);
+
+        if (randomnumber1 < randomnumber2 && randomnumber1 < 50) {
+            System.out.println("Zahl 1 ist kleiner als Zahl 2 und Mini");
+        } else if (randomnumber1 < 30 || randomnumber2 < 30) {
+            System.out.println("Eine der beiden Zahlen ist kleiner als 30");
+        } else if (randomnumber1 < 50 && randomnumber2 != 50) {
+            System.out.println("Erste Zahl klein, zweite kein 50iger");
+        }
+    }
+}
